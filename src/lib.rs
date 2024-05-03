1401936b2c2bf481ae789b9e1141882f1d4f71d2
qbug <wumqing@yeah.net>
2024-05-03 22:57:47 +0800

    test: get_input_config

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialOrd, PartialEq)]
 pub struct InputConfig {
     path: String,
 }
@@ -13,5 +13,38 @@ pub fn get_input_config(args: Vec<String>) -> Result<InputConfig, String> {
     match args.len() {
         0 | 1 => Err("Args is empty!".to_string()),
         _ => Ok(InputConfig::new(args[1].clone())),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_input_config_success() {
+        let args = vec!["input_config.rs".to_string(), "test".to_string()];
+        let result = get_input_config(args);
+        assert_eq!(Ok(InputConfig { path: "test".to_string() }), result);
+    }
+
+    #[test]
+    fn test_get_input_config_no_args() {
+        let args = vec![];
+        let result = get_input_config(args);
+        assert_eq!(Err("Args is empty!".to_string()), result);
+    }
+
+    #[test]
+    fn test_get_input_config_only_one_arg() {
+        let args = vec!["input_config.rs".to_string()];
+        let result = get_input_config(args);
+        assert_eq!(Err("Args is empty!".to_string()), result);
+    }
+
+    #[test]
+    fn test_get_input_config_two_args() {
+        let args = vec!["input_config.rs".to_string(), "test".to_string(), "args".to_string()];
+        let result = get_input_config(args);
+        assert_eq!(Ok(InputConfig { path: "test".to_string() }), result);
     }
 }
