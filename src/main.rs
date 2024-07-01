04bdbd7275c8d280783c98ada188f38cecd6349b
qbug <wumqing@yeah.net>
2024-07-01 20:17:20 +0800

    feat: runnable file moving watcher

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
-use std::path::{Path, PathBuf};
-use notify::{RecursiveMode, Watcher};
-use crate::pipeline::Pipeline;
+use std::path::PathBuf;
+
+use notify::{Event, RecursiveMode, Watcher};
+
 use crate::processor::move_file_processor::MoveFileProcessor;
+use crate::processor::Processor;
 
 mod processor;
 mod pipeline;
 
 fn main() -> Result<(), String> {
-    let processor = MoveFileProcessor::new(PathBuf::from("/home/qbug/tmp/target"));
-    let mut pipeline = Pipeline::new(vec![Box::new(processor)], PathBuf::from("/home/qbug/tmp"));
-    pipeline.start();
+    let path = "/home/qbug/tmp";
+    let processor = MoveFileProcessor::new(PathBuf::from(path).join("target"));
+    let mut watcher = notify::recommended_watcher(move |res: notify::Result<Event>| {
+        match res{
+            Ok(event) => {
+                if event.kind.is_create() {
+                    let _ = processor.proceed(&event.paths[0].clone());
+                }
+            }
+            Err(e) => println!("watch error: {:?}", e),
+        }
+    }).unwrap();
+    watcher.watch(path.as_ref(), RecursiveMode::Recursive).unwrap();
     loop {
         println!("sleep...");
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
-
-    Ok(())
 }
--- src/pipeline.rs
+++ src/pipeline.rs
@@ -1,42 +1,17 @@
-use std::path::{Path, PathBuf};
-use notify::{RecommendedWatcher, RecursiveMode, Watcher};
-use crate::processor::move_file_processor::MoveFileProcessor;
+use std::path::PathBuf;
+
 use crate::processor::Processor;
 
 pub struct Pipeline {
     processors: Vec<Box<dyn Processor>>,
     watch_path: PathBuf,
-    watcher: RecommendedWatcher
 }
 
 impl Pipeline {
     pub fn new(processors: Vec<Box<dyn Processor>>, watch_path: PathBuf) -> Self {
-        let watcher = notify::recommended_watcher(|res| {
-            match res {
-                Ok(event) => {
-                    if event.kind.is_create() {
-                        for processor in processors {
-                            processor.proceed(event.paths[0].clone());
-                        }
-                    }
-                }
-                Err(e) => println!("watch error: {:?}", e),
-            }
-        })?;
         Self {
             processors,
             watch_path,
-            watcher
         }
-    }
-
-    pub fn stop(&mut self) {
-        // stop watcher
-        self.watcher.unwatch(Path::new(&self.watch_path)).unwrap();
-    }
-
-    pub fn start(&mut self) {
-        // use rust notify new version to watch if dir has changed
-        self.watcher.watch(Path::new(&self.watch_path), RecursiveMode::Recursive)?;
     }
 }
